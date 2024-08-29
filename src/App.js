2c1d29343b88d0892bc1b81c6466597b956fcbc6
atekelt <atekeltafework55@gmail.com>
2024-08-29 13:24:57 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MenuManagementPage from './pages/MenuManagementPage';
+
+function App() {
+  return (
+    <div className="App">
+        <MenuManagementPage />
+    </div>
+);
+}
+
+export default App;
